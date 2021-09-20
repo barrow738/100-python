01af293cae65db0eeb7b4b349b26266fb03651b2
barrow738 <57837415+barrow738@users.noreply.github.com>
2021-09-20 06:55:17 +0300

    Leaning about turtle

---
+++ paul_turtle.py
@@ -0,0 +1,11 @@
+import turtle
+
+paul_turtle = turtle.Turtle()
+paul_turtle.forward(100)
+paul_turtle.right(90)
+paul_turtle.forward(100)
+paul_turtle.right(90)
+paul_turtle.forward(100)
+paul_turtle.right(90)
+paul_turtle.forward(100)
+
